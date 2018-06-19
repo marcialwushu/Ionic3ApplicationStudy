b1b281739d61f2af90bf534a823859621aaabc35
marcialwushu <marcialwushu@gmail.com>
2018-06-19 17:48:09 -0300

    xml configuration, logo and splash screen added resources

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { timer } from 'rxjs/observable/timer';
+
 //import { HomePage } from '../pages/home/home';
 @Component({
   templateUrl: 'app.html'
@@ -10,12 +12,16 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 export class MyApp {
   rootPage:any = 'LoginPage';
 
+  showSplash = true;
+
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+
+      timer(3000).subscribe(() => this.showSplash = false)
     });
   }
 }
