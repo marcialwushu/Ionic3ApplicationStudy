fdd8e7b4987cf9a9111b37c7f5f81aa35f4b6edb
marcialwushu <marcialwushu@gmail.com>
2018-06-25 17:07:42 -0300

    login, register, getUserInfo and logout fuction added in provider

--- src/providers/auth-service/auth-service.ts
+++ src/providers/auth-service/auth-service.ts
@@ -17,11 +17,29 @@ export class AuthServiceProvider {
   currentUser: User;
 
   public login(credentials) {
-
+    if(credentials.email === null || credentials.password === null) {
+      return Observable.throw("Please insert credentials");
+    }else {
+      // Neste ponto, faça um pedido ao seu back-end para fazer uma verificação real!
+      return Observable.create(observer => {
+        let acess = (credentials.password === "pass" && credentials.email === "email")
+        this.currentUser = new User('Jack', 'jack@email.com');
+        observer.next(acess);
+        observer.complete();
+      });
+    }
   }
 
   public register(credentials) {
-
+    if(credentials.email === null || credentials.password === null) {
+      return Observable.throw("Please insert credentials");
+    } else {
+      // Neste ponto, armazene as credenciais no seu back-end!
+      return Observable.create(observer => {
+        observer.next(true);
+        observer.complete();
+      });
+    }
   }
 
   public getUserInfo() : User {
@@ -29,7 +47,11 @@ export class AuthServiceProvider {
   }
 
   public logout(){
-
+    return Observable.create(observer => {
+      this.currentUser = null;
+      observer.next(true);
+      observer.complete();
+    });
   }
 
 
