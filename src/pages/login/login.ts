352734e6503b5c487f8fa02b93feff77f6029e67
marcialwushu <marcialwushu@gmail.com>
2018-06-26 16:28:26 -0300

    LoginPage methods added

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { HomePage } from '../home/home';
+import { Component, ErrorHandler } from '@angular/core';
+import { IonicPage, NavController, NavParams, Loading, AlertController, LoadingController } from 'ionic-angular';
 import { GooglePlus } from '@ionic-native/google-plus';
+import { AuthServiceProvider } from '../../providers/auth-service/auth-service';
+
 
 
 @IonicPage()
@@ -13,16 +14,61 @@ export class LoginPage {
 
   isUserLoggedIn: any = false;
   userInfo: any = {};
+  loading: Loading;
+  registerCredentials = { email: '', password: '' };
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public gp: GooglePlus) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public gp: GooglePlus,
+    private auth: AuthServiceProvider,
+    private alertCtrl: AlertController,
+    private loadingCtrl: LoadingController
+  ) {
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad LoginPage');
+  public createAccount(){
+    this.navCtrl.push('RegisterPage');
   }
 
-  login(){
-    this.navCtrl.setRoot(HomePage);
+  public login() {
+    this.showLoading()
+    this.auth.login(this.registerCredentials).subscriber(allowed => {
+      if(allowed) {
+        this.navCtrl.setRoot('HomePage');
+      } else {
+        this.showError("Acess Denied");
+      }
+      error => {
+        this.showError(error);
+      }
+
+    })
+  }
+
+  showLoading(){
+    this.loading = this.loadingCtrl.create({
+      content: 'Please wait...',
+      dismissOnPageChange: true
+    });
+    this.loading.present();
+  }
+
+  showError(text){
+    this.loading.dismiss();
+
+    let alert = this.alertCtrl.create({
+      title: 'Fail',
+      subTitle: text,
+      buttons: ['OK']
+    });
+    alert.present(prompt);
+  }
+
+
+  /**
+  Login(){
+    this.navCtrl.setRoot('HomePage');
 
   }
 
@@ -33,6 +79,8 @@ export class LoginPage {
   signUp(){
 
   }
+
+  **/
 
   loginWithGP(){
     this.gp.login({}).then(res => {
